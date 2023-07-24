ed183a4069744f8988891071a0880ad3be426a49
shalev1111 <50710665+shalevofir21@users.noreply.github.com>
2023-07-24 18:52:36 +0300

    Delete classes directory

--- classes/coin.py
+++
@@ -1,5 +0,0 @@
-from classes.object import Object
-class Coin(Object):
-    def __init__(self, size, location, picture, height, point_value):
-        Object.__init__(self, size, location, picture, height)
-        self.point_value = point_value
